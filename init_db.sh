97a2bf55a271cedd6e7eaa0f72cc26ccf65549cf
Luigi Di Fraia <luigidifraia@gmail.com>
2019-07-09 09:31:49 +0000

    Create dc_user as superuser with login permissions too; init the DC DB

--- init_db.sh
+++ init_db.sh
@@ -5,8 +5,10 @@ set -eux
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #Demo PostgreSQL Database initialisation
-psql postgres -c "CREATE USER dc_user PASSWORD 'localuser1234'"
+psql postgres -c "CREATE USER dc_user PASSWORD 'localuser1234' SUPERUSER LOGIN"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
 createdb -O dc_user datacube
 
 #psql -d datacube -U dc_user -f $THISDIR/seed_db.sql
+source activate cubeenv
+datacube system init
