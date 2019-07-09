0328b715d8abfbfb8000f2928005581f152e0ca1
Luigi Di Fraia <luigidifraia@gmail.com>
2019-07-09 09:50:21 +0000

    Auto extract DB details

--- init_db.sh
+++ init_db.sh
@@ -4,11 +4,14 @@ set -eux
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#Demo PostgreSQL Database initialisation
-psql postgres -c "CREATE USER dc_user PASSWORD 'localuser1234' SUPERUSER LOGIN"
-#The -O flag below sets the user: createdb -O DBUSER DBNAME
-createdb -O dc_user datacube
+DATABASE=$( grep db_database $THISDIR/.datacube.conf | awk '{ print $2 }' )
+USERNAME=$( grep db_username $THISDIR/.datacube.conf | awk '{ print $2 }' )
+PASSWORD=$( grep db_password $THISDIR/.datacube.conf | awk '{ print $2 }' )
+
+# PostgreSQL Database initialisation
+psql postgres -c "CREATE USER $USERNAME PASSWORD '$PASSWORD' SUPERUSER LOGIN"
+createdb -O $USERNAME $DATABASE
+
+datacube system init
 
 #psql -d datacube -U dc_user -f $THISDIR/seed_db.sql
-source activate cubeenv
-datacube system init
