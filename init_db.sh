91afa90074101f518a10beb576a744f02b5030d8
Luigi Di Fraia <luigidifraia@gmail.com>
2019-07-09 08:39:38 +0000

    Try a different approach

---
+++ init_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eux
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#Demo PostgreSQL Database initialisation
+psql postgres -c "CREATE USER testuser PASSWORD 'testpass'"
+#The -O flag below sets the user: createdb -O DBUSER DBNAME
+createdb -O dc_user datacube
+
+#psql -d datacube -U dc_user -f $THISDIR/seed_db.sql
