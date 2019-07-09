2a945657a94670ea860733d37c4b276bdc098b61
Luigi Di Fraia <luigidifraia@gmail.com>
2019-07-09 08:58:41 +0000

    Create dc_user

--- init_db.sh
+++ init_db.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 set -eux
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #Demo PostgreSQL Database initialisation
-psql postgres -c "CREATE USER testuser PASSWORD 'testpass'"
+psql postgres -c "CREATE USER dc_user PASSWORD 'localuser1234'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
 createdb -O dc_user datacube
 
