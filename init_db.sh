9588c1fdedd60e92e3d6f08caa257d3d8b106be2
Luigi Di Fraia <luigidifraia@gmail.com>
2019-07-09 10:10:05 +0000

    A few rationalizations

--- init_db.sh
+++ init_db.sh
@@ -2,16 +2,18 @@
 
 set -eux
 
+# PostgreSQL Database initialisation
+
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DATABASE=$( grep db_database $THISDIR/.datacube.conf | awk '{ print $2 }' )
 USERNAME=$( grep db_username $THISDIR/.datacube.conf | awk '{ print $2 }' )
 PASSWORD=$( grep db_password $THISDIR/.datacube.conf | awk '{ print $2 }' )
 
-# PostgreSQL Database initialisation
 psql postgres -c "CREATE USER $USERNAME PASSWORD '$PASSWORD' SUPERUSER LOGIN"
 createdb -O $USERNAME $DATABASE
 
 datacube system init
 
-#psql -d datacube -U dc_user -f $THISDIR/seed_db.sql
+#Further Database preparation, e.g. product addition and dataset indexing
+#psql -d $DATABASE -U $USERNAME -f $THISDIR/seed_db.sql
